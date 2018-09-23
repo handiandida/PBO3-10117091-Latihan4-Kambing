91dd345c18408ade05b9a8006f23fc8d756d90a3
Dida Handian <handiandida@gmail.com>
2018-09-23 19:33:15 +0700

    Kambing

---
+++ src/pbo3/pkg10117091/latihan4/kambing/PBO310117091Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan4.kambing;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini bisa Menambahkan Jumlah Kambing
+*/
+public class PBO310117091Latihan4Kambing {
+            public void tambahKambing(){
+    // Deklarasi variabel lokal
+    int jumlahKambing = 0;
+    
+    jumlahKambing = jumlahKambing + 5;
+    System.out.println("Jumlah Kambing setelah ditambah: " + 
+            jumlahKambing);
+            }
+
+    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO310117091Latihan4Kambing kambingJantan = new 
+        PBO310117091Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
